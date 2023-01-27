49992eab3d42ebec925c7e97b27a07ae0a6c3ea2
Denzelle Walker <denzellew@gmail.com>
2023-01-27 12:33:45 -0500

    Updated backend test

--- index.ts
+++ index.ts
@@ -1,47 +1,75 @@
-
 import { personalSign } from "@metamask/eth-sig-util";
-import * as sha256 from '@stablelib/sha256';
-import { mnemonicToSeed, entropyToMnemonic } from '@ethersproject/hdnode';
-import { fromHex } from '@3id/common';
-import { fromString, toString } from 'uint8arrays';
+import * as sha256 from "@stablelib/sha256";
+import { mnemonicToSeed, entropyToMnemonic } from "@ethersproject/hdnode";
+import { fromHex } from "@3id/common";
+import { fromString, toString } from "uint8arrays";
 import { ThreeIdProvider } from "@3id/did-provider";
+import { getResolver as get3IDResolver } from '@ceramicnetwork/3id-did-resolver'
 import { CeramicClient } from "@ceramicnetwork/http-client";
+import { DID } from 'dids'
+import { TileDocument } from "@ceramicnetwork/stream-tile";
+import { getResolver as getKeyResolver } from 'key-did-resolver'
 
-
+const testDID = "did:3:kjzl6cwe1jw147x3oysta3ikhvczpiyzktzq5wzx1qeguyjeqis6pqukgt1ldj3"
+const testAccount = "0x3B6F73C70B3487e7Da7efF6bc5c2cE1995E36F8d";
+const testPrivateKey = "f1cef72665e8822a38b6dd4a21649b26739c7d72336140577bf38a710540f57e";
+const testTile = "disco-public-user-data-test";
+const testBio = "gm frens! I'm Evin.\n\nI'm so excited you're here, let's dance!";
 
 async function get3ID() {
-    const message = 'Allow this account to control your identity'
-    const authSecret = authenticate(message);
-    const entropy = sha256.hash(fromString(authSecret.slice(2)))
-    // const message2 = 'This app wants to view and update your 3Box profile.'
+  const message = "Allow this account to control your identity";
+  const authSecret = authenticate(message);
+  const entropy = sha256.hash(fromString(authSecret.slice(2)));
+  // const message2 = 'This app wants to view and update your 3Box profile.'
+  const ceramic = new CeramicClient("https://ceramic-single.disco.xyz");
 
-    const threeID = await ThreeIdProvider.create({
-        authId: '0x2362Ba4Cc274E7047529f92F54937555EEf9d444',
-        authSecret: entropy,
-        ceramic: new CeramicClient("https://ceramic-single.disco.xyz"),
-        // See the section above about permissions management
-        getPermission: (request) => Promise.resolve(request.payload.paths),
-      })
+  const threeID = await ThreeIdProvider.create({
+    authId: testAccount,
+    authSecret: entropy,
+    ceramic,
+    // See the section above about permissions management
+    getPermission: (request) => Promise.resolve(request.payload.paths),
+  });
 
-      console.log(threeID.id);
+  const did = new DID({
+    provider: threeID.getDidProvider(),
+    resolver: {
+      ...get3IDResolver(ceramic),
+      ...getKeyResolver(),
+    },
+  })
+
+  // Authenticate the DID using the 3ID provider
+  await did.authenticate()
+
+  ceramic.did = did;
+  const didID = threeID.id;
+
+  console.log("isTestDid", testDID == didID);
+
+  const metadata = {
+    controllers: [didID],
+    family: testTile
+  };
+  
+  const tile = await TileDocument.deterministic<any>(ceramic, metadata);
+  await tile.update({...tile.content, ...{profile: {...tile.content.profile, ...{bio: testBio}}}})
+  console.log(tile);
 }
 
 function authenticate(message: string) {
-    const key = "1a2aa172f7ab996164a0a54ebfcd37865363f1219f337eade74df0aeab2e9dd0"
-    const privateKey = Buffer.from(
-        key,
-        'hex',
-      );
+  const key = testPrivateKey;
+  const privateKey = Buffer.from(key, "hex");
 
-      // Authenticate
-      const signature = personalSign({
-        privateKey,
-        data: message
-      });
+  // Authenticate
+  const signature = personalSign({
+    privateKey,
+    data: message,
+  });
 
-      const signatureBytes = fromString(signature.slice(2))
-      const digest = sha256.hash(signatureBytes)
-      return `0x${toString(digest, 'base16')}`;
+  const signatureBytes = fromString(signature.slice(2));
+  const digest = sha256.hash(signatureBytes);
+  return `0x${toString(digest, "base16")}`;
 }
 
 get3ID().then();
